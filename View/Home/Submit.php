b776234731eddac88769895ab34931bd38678890
amirmahdibaharlou <amir.mbaharlou@yahoo.com>
2020-07-31 13:28:05 +0430

    Added Submit to Psot

---
+++ View/Home/Submit.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="<?php echo _Root ?>static/css/submit.css">
+
+
+<form class="submit-form submit-text">
+    <h1>ارسال پست</h1>
+
+    <label>* نام و نام خانوادگی :</label>
+    <input type="text" name="" value="" placeholder="نام و نام خانوادگی" >
+
+    <label>* ایمیل :</label>
+    <input type="email" name="" value="" placeholder="ایمیل" >
+    
+    <label>وب سایت :</label>
+    <input type="site" name="" value="" placeholder="وب سایت" >
+
+    <label>* متن یا لینک :</label>
+    <textarea rows="10"></textarea>
+    
+    <a href="#" class="border-radius background-gold color-dark button-medium" >ارسال</a>
+
+</form>
